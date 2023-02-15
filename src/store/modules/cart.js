a2dbb0b18bbd530eeb0d9286c5238621468c905c
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-14 19:41:22 -0500

    migrating to vuex

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,4 +1,5 @@
 export default {
+  namespaced: true,
   state() {
     return {
       items: [],
@@ -8,7 +9,7 @@ export default {
   },
   mutations: {
     addProductToCart(state, payload) {
-      const productData = payload.product;
+      const productData = payload;
       const productInCartIndex = state.items.findIndex(
         (ci) => ci.productId === productData.id
       );
@@ -28,10 +29,8 @@ export default {
       state.qty++;
       state.total += productData.price;
     },
-
     removeProductFromCart(state, payload) {
       const prodId = payload.productId;
-
       const productInCartIndex = state.items.findIndex(
         (cartItem) => cartItem.productId === prodId
       );
@@ -43,7 +42,10 @@ export default {
   },
   actions: {
     addToCart(context, payload) {
-      context.commit('addProductToCart', payload);
+      const prodId = payload.id;
+      const products = context.rootGetters['prods/products'];
+      const theProduct = products.find((prod) => prod.id === prodId);
+      context.commit('addProductToCart', theProduct);
     },
     removeFromCart(context, payload) {
       context.commit('removeProductFromCart', payload);
@@ -57,9 +59,7 @@ export default {
       return state.total;
     },
     quantity(state) {
-      return state.quantity;
+      return state.qty;
     },
   },
 };
-
-// cart: { items: [], total: 0, qty: 0 },
